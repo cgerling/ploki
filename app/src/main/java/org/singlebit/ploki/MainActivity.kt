7e4672ab8a57612df0f0e244c5faaacf88c9cdbe
Calvin Gerling <calvin.gerling@singlebit.org>
2019-12-23 18:51:55 -0300

    initial commit

---
+++ app/src/main/java/org/singlebit/ploki/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.singlebit.ploki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
