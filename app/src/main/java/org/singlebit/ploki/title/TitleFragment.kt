2264f324b8adcb4d326c109d1cfd46aed3c48271
Calvin Gerling <calvin.gerling@singlebit.org>
2019-12-23 20:03:35 -0300

    feat: create action to navigate between title and search fragments

--- app/src/main/java/org/singlebit/ploki/title/TitleFragment.kt
+++ app/src/main/java/org/singlebit/ploki/title/TitleFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_title.view.*
 
 import org.singlebit.ploki.R
 
@@ -14,7 +16,13 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        val view = inflater.inflate(R.layout.fragment_title, container, false)
+
+        view.search_view.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_searchFragment)
+        )
+
+        return view
     }
 
 }
